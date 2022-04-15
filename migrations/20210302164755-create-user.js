f45f3afea46215e832b00316ee96caf27bd91850
TylusDawkins <100214696+TylusDawkins@users.noreply.github.com>
2022-04-15 08:36:45 -0700

    added auth, not tested

---
+++ migrations/20210302164755-create-user.js
@@ -0,0 +1,40 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      passwordDigest: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users')
+  }
+}
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const AuthRouter = require('./routes/AuthRouter')
-const PostRouter = require('./routes/PostRouter')
 const app = express()
 
 const PORT = process.env.PORT || 3001
@@ -13,6 +12,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 
 app.use('/auth', AuthRouter)
-app.use('/posts', PostRouter)
+app.use('/', AuthRouter)
 
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
