d00615747cd357b1d60c9b6cfd68911803a0432b
TylusDawkins <100214696+TylusDawkins@users.noreply.github.com>
2022-04-22 08:57:49 -0700

    final heroku deploy hopefully

--- migrations/20220419194845-create-review.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('reviews') 
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('reviews');
-  }
-};
