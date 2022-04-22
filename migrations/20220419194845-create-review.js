02b4cf9776a46410cbba2229e423ab1fe1ca6a0b
TylusDawkins <100214696+TylusDawkins@users.noreply.github.com>
2022-04-22 08:55:24 -0700

    fixing heroku

--- migrations/20220419194845-create-review.js
+++ migrations/20220419194845-create-review.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('reviews', 
+    await queryInterface.createTable('reviews') 
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('reviews');
